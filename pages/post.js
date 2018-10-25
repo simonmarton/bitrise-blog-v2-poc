3df47d4a77ed578b4503c3a6ee409ac16483b6d8
Simon Márton <simon.marton@gmail.com>
2018-10-25 15:44:09 +0200

    poc

--- pages/post.js
+++ pages/post.js
@@ -2,25 +2,26 @@ import React from 'react'
 import Butter from 'buttercms'
 import Head from 'next/head'
 
-const butter = Butter('de55d3f93789d4c5c26fb07445b680e8bca843bd')
-
 export default class extends React.Component {
   static async getInitialProps({ query }) {
-    const resp = await butter.post.retrieve(query.slug);  
-    return resp.data;
+    const butter = Butter(process.env.BUTTER_TOKEN);
+    const { data : {data: post}} = await butter.post.retrieve(query.slug);  
+    
+    return {post};
   }
+
   render() {
-    const post = this.props.data;
+    const {post} = this.props;
 
     return (
-      <div>
+      <div className="post-content">
         <Head>
           <title>{post.seo_title}</title>
           <meta name="description" content={post.meta_description} />
           <meta name="og:image" content={post.featured_image} />
         </Head>
 
-        <h1>{post.title}</h1>
+        <h1 className="post-title">{post.title}</h1>
         <div dangerouslySetInnerHTML={{__html: post.body}} />
       </div>
     )
